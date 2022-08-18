4911b65eb1201929422c05dad2867866abb30084
Siddhi Wanzkhade <87324913+siddhiwanzkhade@users.noreply.github.com>
2022-08-19 00:27:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import requests
+from datetime import datetime
+USERNAME="siddhiwanzkhade"
+TOKEN="sjckje3km667nnl"
+GRAPH_ID="graph1"
+
+pixela_endpoint="https://pixe.la/v1/users"
+
+user_params={
+    "token":TOKEN,
+    "username":USERNAME,
+    "agreeTermsOfService":"yes",
+    "notMinor":"yes"
+
+}
+# response=requests.post(url=pixela_endpoint,json=user_params)
+# print(response.text)
+
+graph_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config={
+    "id":GRAPH_ID,
+    "name":"GRE/Aptitude Study Graph",
+    "unit":"hour",
+    "type":"float",
+    "color":"ajisai"
+}
+headers={
+   "X-USER-TOKEN":TOKEN
+}
+response=requests.post(url=graph_endpoint,json=graph_config,headers=headers)
+print(response.text)
+
+pixel_creation_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today=datetime.now()
+print(today.strftime("%Y%m%d"))
+
+pixela_data={
+    "date":today.strftime("%Y%m%d"),
+    "quantity":input("How many hours did you study  today?")
+}
+
+# response=requests.post(url=pixel_creation_endpoint,json=pixela_data,headers=headers)
+# print(response.text)
+
+update_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data={
+    "quantity":"2.5",
+}
+# response=requests.put(url=update_endpoint,json=new_pixel_data,headers=headers)
+# print(response.text)
+
+delete_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+# response=requests.delete(url=delete_endpoint,headers=headers)
+# print(response.text)
